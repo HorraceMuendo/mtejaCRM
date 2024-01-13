a726641a0bf0e11234d9c408f7c69d775679bf38
HorraceMuendo <horracemuendo34@gmail.com>
2024-01-13 16:52:16 +0300

    project init

---
+++ src/main/java/com/example/mteja/MtejaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mteja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtejaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mteja/MtejaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mteja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
