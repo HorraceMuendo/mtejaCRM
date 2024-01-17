d48d8a66f0367910472c4a77e3c0c5ff4294196c
HorraceMuendo <horracemuendo34@gmail.com>
2024-01-17 16:32:49 +0300

    Testing out the first endpoint

--- src/main/java/com/example/mteja/Contoller/UserContoller.java
+++ src/main/java/com/example/mteja/Contoller/UserContoller.java
@@ -1,29 +1,38 @@
 package com.example.mteja.Contoller;
 
+import com.example.mteja.Model.User;
 import com.example.mteja.Repository.userRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("mteja/users")
+@RequestMapping("/mteja")
 public class UserContoller {
     @Autowired
     public userRepo UserRepo;
 
     @GetMapping("users")
-    public ResponseEntity getUsers(){
-
-        return null;
+    public ResponseEntity<List<User>> getUsers(){
+        List<User> users = UserRepo.findAll();
+        return new ResponseEntity(users, HttpStatus.OK);
     }
-    @GetMapping
-    public ResponseEntity getUser(){
-
-        return null;
+    @GetMapping("users/{userID}")
+    public ResponseEntity<User> getUser(@PathVariable Long userID){
+        Optional<User> user = UserRepo.findById(userID);
+        if (user.isPresent()){
+            return new ResponseEntity(user.get(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity(HttpStatus.OK);
+        }
     }
 
 }
