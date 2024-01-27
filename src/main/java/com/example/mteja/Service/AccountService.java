783f1618894790255ae570c148a137464ec39826
HorraceMuendo <horracemuendo34@gmail.com>
2024-01-27 14:15:27 +0300

    working on product service interface

---
+++ src/main/java/com/example/mteja/Service/AccountService.java
@@ -0,0 +1,5 @@
+package com.example.mteja.Service;
+
+public interface AccountService {
+
+}
---
+++ src/main/java/com/example/mteja/Service/productService.java
@@ -0,0 +1,8 @@
+package com.example.mteja.Service;
+
+import java.util.Optional;
+
+public interface productService {
+  Product addProduct(Product product);
+  Optional<product>findById
+}
